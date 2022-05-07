0a6136340dd2e12a64014da3992bd836fcdfd8db
Mario <mateasmario@aol.com>
2022-05-07 13:22:21 +0300

    Added base project templates

--- src/main/java/com/example/yuber/HelloApplication.java
+++ src/main/java/com/example/yuber/HelloApplication.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 800);
+        scene.getStylesheets().add(getClass().getResource("css/styles.css").toExternalForm());
+        stage.setTitle("Yuber");
         stage.setScene(scene);
         stage.show();
     }
