50014d6bfbef699d136e077ece8b82e0fde74003
giuliamandres <93037838+giuliamandres@users.noreply.github.com>
2022-05-09 22:00:32 +0300

    registration-page

--- src/main/java/com/example/yuber/Main.java
+++ src/main/java/com/example/yuber/Main.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("base-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("register.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1100, 800);
         scene.getStylesheets().add(getClass().getResource("css/styles.css").toExternalForm());
         stage.setTitle("Yuber");
