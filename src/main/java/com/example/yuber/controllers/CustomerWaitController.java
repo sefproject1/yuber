ec1600b3122a8af1eb03e6996e97061d7fe969b4
Mario <mateasmario@aol.com>
2022-05-22 10:16:48 +0300

    Fix: App now redirects you to a waiting page when a driver accepts your order.

--- src/main/java/com/example/yuber/controllers/CustomerWaitController.java
+++ src/main/java/com/example/yuber/controllers/CustomerWaitController.java
@@ -2,21 +2,16 @@ package com.example.yuber.controllers;
 
 import com.example.yuber.models.OrderSession;
 import com.example.yuber.models.UserSession;
-import com.example.yuber.services.AcceptanceRunnable;
 import com.example.yuber.services.OrderService;
 import com.example.yuber.services.SceneService;
 import com.example.yuber.services.UserService;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.nio.channels.AcceptPendingException;
 
 public class CustomerWaitController {
     @FXML
